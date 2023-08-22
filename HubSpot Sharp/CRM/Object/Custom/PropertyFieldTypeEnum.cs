bf81235e969cb55c0e1b744906e9136790855777
Erik Smit <erik@ibabs.dev>
2023-08-22 11:28:28 +0200

    fix incorrect serialization names for propertyfieldtypes

--- HubSpot Sharp/CRM/Object/Custom/PropertyFieldTypeEnum.cs
+++ HubSpot Sharp/CRM/Object/Custom/PropertyFieldTypeEnum.cs
@@ -21,6 +21,7 @@ namespace HubSpot_Sharp.CRM.Object.Custom
         /// An input that will allow users to select one of either Yes or No. When used in a form, it will be displayed as a single
         /// checkbox.
         /// </summary>
+        [EnumMember(Value = "booleancheckbox")]
         BooleanCheckBox,
 
         /// <summary>
@@ -32,6 +33,7 @@ namespace HubSpot_Sharp.CRM.Object.Custom
         /// <summary>
         /// A list of checkboxes that will allow a user to select multiple options from a set of options allowed for the property.
         /// </summary>
+        [EnumMember(Value = "checkbox")]
         CheckBox,
 
         /// <summary>
@@ -68,6 +70,7 @@ namespace HubSpot_Sharp.CRM.Object.Custom
         /// <summary>
         /// A plain text string, displayed as a multi-line text input.
         /// </summary>
+        [EnumMember(Value = "textarea")]
         TextArea,
 
         /// <summary>
